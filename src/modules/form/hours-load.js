12cd6ad55dea22b247dc353773aeac69edf575e3
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-11-29 21:36:14 -0300

    --feat Showing available hours depending on user hour.

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours.js";
 
+const hours = document.querySelector('#hours')
+
 export function hoursLoad({date}) {
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":")
@@ -14,5 +16,13 @@ export function hoursLoad({date}) {
     }
   })
 
-  console.log(opening)
+  opening.forEach(({hour, available}) => {
+    const li = document.createElement('git')
+    li.classList.add("hour")
+    li.classList.add(available ? 'hour-available' : 'hour-unavailable')
+
+    li.textContent = hour
+
+    hours.append(li)
+  })
 }
