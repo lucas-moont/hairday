06019c7cf0cce4dfb09a3df7435a4da41c21caa5
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-12-01 01:17:29 -0300

    --feat Displaying schedule hours from API.

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleShow } from "./show.js";
 
 const selectedDate = document.querySelector('#date')
 
-export function scheduleDays(){
+export async function scheduleDays(){
   const date = selectedDate.value 
+
+  const dailySchedules = await scheduleFetchByDay({date})
+  console.log(dailySchedules)
+
+  scheduleShow({dailySchedules})
 
   hoursLoad({date})
 }
